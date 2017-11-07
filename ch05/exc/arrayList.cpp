5c99640ea4687455b8bd8dc9fb28886cfaa71a79
kk4728 <gongrong816@126.com>
2017-11-08 06:01:32 +0800

    to be continue.

---
+++ ch05/exc/arrayList.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <ostream>
+#include <algorithm>
+
+
+class illegalParamterValue
+{
+public:
+	illegalParamterValue(const char* pStr) {}
+	illegalParamterValue(std::string str) {}
+
+
+};
+
+template <class T>
+class linearList
+{
+public:
+	virtual ~linearList() {}
+
+	virtual bool empty() const = 0;
+
+	virtual int size() const = 0;
+
+	virtual T& get(int index) const = 0;
+
+	virtual int indexOf(const T& element) const = 0;
+
+	virtual void erase(int index) = 0;
+
+	virtual void insert(int index, const T& element) = 0;
+
+	virtual void output(std::ostream& out) const = 0;
+
+};
+
+
+template <class T>
+class arrayList : public linearList<T>
+{
+public:
+	arrayList(int initCapacity = 10);
+	arrayList(const arrayList<T>& );
+	~arrayList() { delete [] element; }
+
+
+	bool empty() const { return listSize == 0; }
+	
+	int size() const { return listSize; }
+
+	T& get(int index) const ;
+
+	int indexOf(const T& theElement) const ;
+
+	void erase(int index);
+
+	void insert(int index, const T& theElement); 
+
+	void output(std::ostream& out) const;
+
+	int capacity() const { return arrayLength; }
+
+
+protected:
+	T* element;
+	int arrayLength;
+	int listSize;
+
+	void checkIndex(int index) const;
+};
+
+template <class T>
+arrayList<T> :: arrayList(int initCapacity)
+{
+	if(initCapacity <0)
+	{
+		//std::ostringstream s;
+		//s << "Init Capacity = " << initCapacity << "Must be > 0 ";
+		//throw illegalParamterValue(s.c_str());
+		throw illegalParamterValue("111");
+	}
+	arrayLength = initCapacity;
+	element = new T[arrayLength];
+	listSize = 0;
+}
+
+template <class T>
+arrayList<T> :: arrayList(const arrayList<T>& theList)
+{
+	arrayLength = theList.arrayLength;
+	listSize = theList.listSize;
+	element = new T[arrayLength];
+	std::copy(theList.element, theList.element + listSize, element);
+}
+
+template<class T>
+void arrayList<T>::checkIndex(int index) const
+{
+	if(index <0 || index>=listSize)
+	{
+		//throw
+	}
+}
+
+template<class T>
+T& arrayList<T>::get(int index) const
+{
+	checkIndex(index);
+	return element[index];
+}
+
+template<class T>
+int arrayList<T>::indexOf(const T& theElement) const
+{
+	int theIndex = (int) (std::find(element, element+listSize, theElement) - element);
+	if(theIndex == listSize)
+		return -1;
+	else
+		return theIndex;
+}
+
+template<class T>
+void arrayList<T>::erase(int index)
+{
+	checkIndex(index);
+
+	std::copy(element+index+1, element+listSize, element+index);
+
+	element[--listSize].~T(); // delete ? deconstrution 将末尾处的T元素类型进行析构掉
+}
+
+template<class T>
+void arrayList<T>::insert(int index, const T& theElement)
+{
+	if(index<0 || index>listSize)
+	{
+		//throw
+	}
+
+	//索引有效，确认数组是否已满
+	if(listSize == arrayLength)
+	{
+		//数组空间已满，倍增数组长度
+		//...
+
+		arrayLength *= 2;
+	}
+
+	std::copy_backward(element+index, element+listSize, element+listSize+1);
+	
+	element[index] = theElement;
+
+	listSize++;
+}
+
+template<class T>
+void arrayList<T>::output(std::ostream& out) const
+{
+	std::copy(element, element+listSize, std::ostream_iterator<T>(std::cout, " "));	
+}
+
+//overload
+template<class T>
+std::ostream& operator<<(std::ostream& out, const arrayList<T>& x)
+{
+	x.output(out);
+	return out;
+}
+
+class iterator
+{
+public:
+	//typedef bidirectional_iterator_tag interator_category;
+	typedef int INT;
+
+
+protected:
+	
+};
+
+
+void f_test()
+{
+	linearList<int>* x = (linearList<int>*)new arrayList<int>(100);
+//	linearList y = (linearList)new arrayList<int>(100);//error
+
+	arrayList<double> y(100);
+
+	arrayList<char> z;
+
+	arrayList<double> w(y);
+
+
+	y.output(std::cout);
+	std::cout <<std::endl;
+}
+
+
+int main()
+{
+	f_test();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ch06/sec/chain.cpp
@@ -0,0 +1,266 @@
+#include <iostream>
+
+template <class T>
+
+class linearList
+{
+public:
+	virtual ~linearList() {}
+
+	virtual bool empty() const = 0;
+
+	virtual int size() const = 0;
+
+	virtual T& get(int index) const = 0;
+
+	virtual int indexOf(const T& element) const = 0;
+
+	virtual void erase(int index) = 0;
+
+	virtual void insert(int index, const T& element) = 0;
+
+	virtual void output(std::ostream& out) const = 0;
+
+};
+
+
+template <class T>
+struct chainNode
+{
+	T element;
+	chainNode<T>* next;
+
+	chainNode() {}
+	chainNode(const T& element)
+	{
+		this->element = element;
+	}
+
+	chainNode(const T& element, chainNode<T>* next)
+	{
+		this->element = element;
+		this->next = next;
+	}
+};
+
+template <class T>
+class chain : public linearList<T>
+{
+public:
+	chain(int initCapacity = 10);
+	chain(const chain<T>&);
+	~chain();
+
+	virtual bool empty() const { return (listSize == 0);}
+
+	virtual int size() const { return listSize; }
+
+	virtual T& get(int index) const ;
+
+	virtual int indexOf(const T& element) const ;
+
+	virtual void erase(int index) ;
+
+	virtual void insert(int index, const T& element) ;
+
+	virtual void output(std::ostream& out) const ;
+
+protected:
+	void checkIndex(int theIndex) const;
+
+	chainNode<T>* firstNode;
+	int listSize;
+
+};
+
+template< class T>
+chain<T> :: chain(int initCapacity)
+{
+	firstNode = NULL;
+	listSize = 0;
+}
+
+template<class T>
+chain<T>::chain(const chain<T>& theList)
+{
+	listSize = theList.listSize;
+
+	if(listSize ==0 )
+	{
+		firstNode = NULL;
+		listSize = 0;
+	}
+
+	// chain not empty
+	// just for lookup int theList 
+	chainNode<T>* sourceNode = theList.firstNode;
+
+	//deep copy
+	firstNode = new chainNode<T> (sourceNode->element); //point to fist element
+	sourceNode = sourceNode->next;
+
+	// just for lookup int chain
+	chainNode<T>* targetNode = firstNode;
+
+	while( sourceNode != NULL) //deep copy all the elements
+	{
+		targetNode->next = new chainNode<T>(sourceNode->element);
+		targetNode = targetNode->next;
+		sourceNode = sourceNode->next;
+	}
+	targetNode->next = NULL;
+}
+
+template<class T>
+chain<T>::~chain()
+{
+	while(firstNode != NULL)
+	{
+		chainNode<T>* nextNode = firstNode->next;
+		delete nextNode;
+		firstNode = nextNode;
+	}
+}
+
+template<class T>
+void chain<T>::checkIndex(int theIndex) const
+{
+	if(theIndex<0 || theIndex>=listSize)
+	{
+		//throw exception
+	}
+}
+
+template<class T>
+T& chain<T>::get(int index) const
+{
+	checkIndex(index);
+
+	chainNode<T>* curNode = firstNode;
+	for(int i=0; i<index; i++)
+		curNode = curNode->next;
+
+	return curNode->element;
+}
+
+template<class T>
+int chain<T>::indexOf(const T& element) const
+{
+	chainNode<T>* curNode = firstNode;
+	int index = 0;
+	while(curNode != NULL)
+	{
+		index++;
+		if(element == curNode->element)
+		{
+			return index;
+		}
+		curNode = curNode->next;
+	}
+	return -1;
+}
+
+template<class T>
+void chain<T>::erase(int index)
+{
+	checkIndex(index);
+
+	chainNode<T>* deleteNode = NULL;
+	if(index ==0)
+	{
+		deleteNode = firstNode;
+		firstNode = firstNode->next;
+	}
+	else
+	{
+		chainNode<T>* p = firstNode;
+		for(int i=0; i<index-1; i++)
+		{
+			p = p->next; // find the index th element
+		}
+
+		deleteNode = p->next;
+		p->next = p->next->next;
+	}
+
+	listSize--;
+	delete deleteNode;
+}
+
+template<class T>
+void chain<T>::insert(int index, const T& theElement)
+{
+	if(index<0 || index>listSize)
+	{
+		//throw exception
+	}
+	
+	if(index==0)
+	{
+		//after firstNode
+		firstNode = new chainNode<T>(theElement, firstNode);
+	}
+	else
+	{
+		chainNode<T>* p = firstNode;
+		for(int i=0; i<index-1; i++)
+			p = p->next;
+
+		//after p for insert
+		p->next = new chainNode<T>(theElement, p->next);
+
+	}
+	listSize++;
+}
+
+template<class T>
+void chain<T>::output(std::ostream& out) const
+{
+	chainNode<T>* p = firstNode;
+	while(p != NULL)
+	{
+		out << p->element << " " ;
+		p = p->next;
+	}
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& out, const chain<T> & x)
+{
+	x.output(out);
+	return out;
+}
+
+
+
+
+int main()
+{
+	chain<int> c;
+
+	c.insert(0, 1);
+	c.insert(1, 22);
+	c.insert(2, 3);
+	
+	std::cout <<"size: "<< c.size() <<std::endl;
+
+ 	std::cout << " index of" << c.indexOf(33)<< std::endl;
+ 	std::cout << " index of" << c.indexOf(3)<< std::endl;
+
+	std::cout << c;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
